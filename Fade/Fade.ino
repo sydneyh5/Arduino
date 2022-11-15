ac422ea026ee8f1ef41e7565fe735b17b5ed0db6
Sydney <hribar.sydney@gmail.com>
2022-11-15 08:32:40 -0500

    Change from Pin 9 to ~11 PWM
    PWM - Pulse Width Modulation

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -13,19 +13,19 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
 */
 
-int led = 9;           // the PWM pin the LED is attached to
+int led = 11;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // declare pin 9 to be an output:
+  // declare pin 11 to be an output:
   pinMode(led, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
+  // set the brightness of pin 11:
   analogWrite(led, brightness);
 
   // change the brightness for next time through the loop:
