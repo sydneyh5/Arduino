9a4818460d36a51afc2e1e2031e8085822a55376
Sydney <hribar.sydney@gmail.com>
2022-11-15 07:56:24 -0500

    Add Pin 11 LED Blink alongside PIN 12

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -8,14 +8,17 @@ Program: Blink
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 12 as an output.
+  // initialize digital pin 12 and 11 as an output.
   pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
+  delay(150);                       // wait for a second
   digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                       // wait for a second
-}
+  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(150);                       // wait for two seconds
+  digitalWrite(11, LOW);    // turn the LED off by making the voltage 
+} 
