015882265ac05528fccb7ae1d206eba3adb648eb
Sydney <hribar.sydney@gmail.com>
2022-11-30 07:57:47 -0500

    Initial Data

--- TemperatureHumiditySensor/TemperatureHumiditySensor.ino
+++ TemperatureHumiditySensor/TemperatureHumiditySensor.ino
@@ -1,3 +1,13 @@
+
+/*
+ * Programmer: Sydney Hribar
+ * 11.30.22
+ * Program: Temperature and humidity sensor
+ * Resource: https://create.arduino.cc/projecthub/ThothLoki/portable-arduino-temp-humidity-sensor-with-lcd-a750f4?ref=platform&ref_id=424_trending__beginner_&offset=0
+ */
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
