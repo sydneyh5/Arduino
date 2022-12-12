d91dd5f5a37d93aef90ccb138ae02000fdfa175d
Sydney <hribar.sydney@gmail.com>
2022-12-12 07:48:12 -0500

    Initial USS Commit

---
+++ UltrasonicSecuritySystem/UltrasonicSecuritySystem.ino
@@ -0,0 +1,7 @@
+/*
+ * Programmer: Sydney Hribar
+ * Date: 12.12.2022
+ * Program: Ultrasonic Security System
+ * 
+ * Resource: https://create.arduino.cc/projecthub/Krepak/ultrasonic-security-system-3afe13?ref=search&ref_id=red%20green%20yellow%20light%20echo%20sensor&offset=1
+ */
